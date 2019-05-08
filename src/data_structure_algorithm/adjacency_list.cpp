73b85671f25a06ad4916b466711836fb3a3c7a40
behnam <behnam.asadi@gmail.com>
2019-05-08 04:55:17 +0200

    data_structure_algorithm re-ordered.

--- src/data_structure_algorithm/adjacency_list.cpp
+++ src/data_structure_algorithm/adjacency_list.cpp
@@ -1,1 +1,2 @@
 //https://www.programiz.com/dsa/graph-adjacency-list
+int main()
--- src/timers.cpp
+++ src/timers.cpp
@@ -6,29 +6,59 @@
  */
 
 
-#include <stdio.h>
 #include <iostream>
 #include <time.h>
-using namespace std;
 
-//int main()
-int main()
+
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <thread>
+
+
+template<typename Clock, typename Duration>
+std::ostream &operator<<(std::ostream &stream,  const std::chrono::time_point<Clock, Duration> &time_point)
 {
-/*  get start time  */
+    const time_t time = Clock::to_time_t(time_point);
+    struct tm tm;
+    localtime_r(&time, &tm);
+    return stream << std::put_time(&tm, "%c"); // Print standard date&time
+ 
+}
+
+
+void timeLapseFirstExample()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    std::this_thread::sleep_for(std::chrono::microseconds(1000));  
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+    std::cout<<"time lapse in microseconds:" <<microseconds <<std::endl;
+}
+
+
+void timeLapseSecondExample()
+{
+    /*  get start time  */
     time_t  start_time = clock();
 /*code to measure*/
 	for(int i=0;i<1000000;i++)
 	{
-/*		i*i;
 		i*i;
 		i*i;
 		i*i;
-		i*i;*/
-		cout<<i*i<<endl;
-//		cout<<i*i<<endl;
+		i*i;
+		i*i;
 	}
     float time1 = (float) (clock() - start_time) / CLOCKS_PER_SEC;
     printf("time for code was %f seconds\n", time1);
+}
+
+
+int main()
+{
+//     timeLapseFirstExample();
+    timeLapseSecondExample();
     return 0;
 }
 
